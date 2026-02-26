180fa27abaebc9c1d2dfa77e24c5d65554f1b920
Prashant Pooboni <ppkumar.email@gmail.com>
2026-02-25 21:30:34 -0800

    bench mark - added time compar

--- test/test_resnet_engine.py
+++ test/test_resnet_engine.py
@@ -8,6 +8,7 @@ import json
 from torchvision.models import ResNet50_Weights
 import torchvision.models as models
 import os
+import time
 
 
 # Compare ResNet-50 TensorRT engine results with un-quantized model ResNet-50 
@@ -36,15 +37,30 @@ image_data = BytesIO(response.content)
 img = Image.open(image_data).convert('RGB')
 input_tensor = preprocess(img).unsqueeze(0).cuda()
 
-# Resnet vanilla infrence
+# warm up
+for _ in range(5) : 
+    _ = pt_model(input_tensor)
+torch.cuda.synchronize()
+
+start_event = torch.cuda.Event(enable_timing=True)
+end_event = torch.cuda.Event(enable_timing=True)
+
+#----------- Resnet vanilla infrence --------------
+start_event.record()
+
 with torch.no_grad():
     pt_output = pt_model(input_tensor)
+
+# record time    
+end_event.record()
+torch.cuda.synchronize()
+pt_time = start_event.elapsed_time(end_event)
 
 pt_probs = torch.nn.functional.softmax(pt_output, dim=1)
 pt_conf, pt_idx = torch.max(pt_probs, 1)
 
 
-# TensorRT code
+#-------------- TensorRT code ------------------
 
 TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 with open("../enginefiles/resnet50fp16.engine", 'rb') as f:
@@ -62,9 +78,19 @@ context.set_input_shape(input_name, input_tensor.shape)
 context.set_tensor_address(input_name, input_tensor.data_ptr())
 context.set_tensor_address(output_name, output_tensor.data_ptr())
 
-stream = torch.cuda.current_stream()
-success = context.execute_async_v3(stream_handle=stream.cuda_stream)
-stream.synchronize()
+# create stream
+stream = torch.cuda.current_stream().cuda_stream
+
+for _ in range(5): 
+    context.execute_async_v3(stream) # Warm up
+torch.cuda.synchronize()
+ 
+start_event.record() # start time
+success = context.execute_async_v3(stream)
+end_event.record()
+torch.cuda.synchronize()
+
+trt_time = start_event.elapsed_time(end_event) # record time
 
 prob = torch.nn.functional.softmax(output_tensor, dim=1)
 conf, idx = torch.max(prob, 1)
@@ -77,15 +103,23 @@ if success:
     weights = ResNet50_Weights.DEFAULT
     categories = weights.meta['categories']
 
-    print(f"Vanilla Model infrence Predicated Name {categories[pt_idx]}. Confidence : {pt_conf.item()*100: 2f} ")
-    print(f'Predicted Name : {categories[idx.item()]}. Confidence : {conf.item()*100: 2f}')
+    print(f"Pytorch: total-time {pt_time:6.2f} ms. Label: {categories[pt_idx]}. Confidence : {pt_conf.item()*100: 2f} ")
+    print(f'TensorRT : total-time {trt_time:6.2f} ms Label : {categories[idx.item()]}. Confidence : {conf.item()*100: 2f}')
 else:
     print("Failed to run infrence code")
 print('-'*50)
 
 # Output
-#--------------------------------------------------
-#Inference complete....
-#Vanilla Model infrence Predicated Name golden retriever. Confidence :  90.125465
-#Predicted Name : golden retriever. Confidence :  89.988774
-#--------------------------------------------------
+# --------------------------------------------------
+# Inference complete....
+# Pytorch: total-time  21.98 ms. Label: golden retriever. Confidence :  90.125465
+# TensorRT : total-time   4.26 ms Label : golden retriever. Confidence :  89.988774
+# --------------------------------------------------
+
+# TensorRT optimized model better predication than Pytorch
+# image - Credit : 'dinosaurpictures.org/Ankylosaurus/Ankylosaurus_775883a1.jpg'
+# --------------------------------------------------
+# Inference complete....
+# Pytorch: total-time  21.89 ms. Label: armadillo. Confidence :  35.777852
+# TensorRT : total-time   4.28 ms Label : triceratops. Confidence :  51.548177
+# --------------------------------------------------
