cff38ce5ce71446ef6213f19cfd62bcb12b4df5b
Prashant Pooboni <ppkumar.email@gmail.com>
2026-03-25 22:50:35 -0700

    added cls names

--- 04_object_detect_video.py
+++ 04_object_detect_video.py
@@ -4,6 +4,22 @@ import cv2
 from ultralytics import YOLO
 import os
 
+class_names = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train',
+    'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
+    'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard',
+    'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+    'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork',
+    'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+    'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair',
+    'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+    'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+    'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+    'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
 # Load your TensorRT engine
 model = YOLO("./tensorrt_engine/yolov8n.engine", task="detect")
 
@@ -38,8 +54,9 @@ while True:
     results = model(frame, stream=True)
 
     for r in results:
-        annotated_frame = r.plot()
-        cv2.imshow("Jetson Orin Video Test", annotated_frame)
+      r.names = class_names
+      annotated_frame = r.plot()
+      cv2.imshow("Object detection - Prashant", annotated_frame)
 
     # 4. The Exit Logic (Crucial!)
     if cv2.waitKey(1) == 27: # Press ESC to exit
